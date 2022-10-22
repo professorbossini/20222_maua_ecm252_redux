aea82c53e35d5258b77ba01ea3fa8cc6a132b4cb
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-22 11:03:16 -0300

    apostila09(redux): fim da secao 2.10
    implementação dos reducers

--- index.js
+++ index.js
@@ -25,3 +25,27 @@ const solicitarCashback = (nome, valor) => {
     }
   }
 }
+
+//essa função é um reducer
+const historicoDePedidosDeCashback = (historicoDePedidosDeCashbackAtual = [], acao) => {
+  if (acao.type === "CASHBACK"){
+    return [...historicoDePedidosDeCashbackAtual, acao.payload]  
+  }
+  return historicoDePedidosDeCashbackAtual  
+}
+//essa função é um reducer
+const caixa = (dinheiroEmCaixa = 0, acao) => {
+  if (acao.type === "CASHBACK")
+    dinheiroEmCaixa -= acao.payload.valor
+  else if (acao.type === "CRIAR_CONTRATO")
+    dinheiroEmCaixa += acao.payload.taxa
+  return dinheiroEmCaixa
+}
+//essa função é um reducer
+const contratos = (listaDeContratoAtual = [], acao) => {
+  if (acao.type === "CRIAR_CONTRATO")
+    return [...listaDeContratoAtual, acao.payload]
+  if (acao.type === "CANCELAR_CONTRATO")
+    return listaDeContratoAtual.filter(c => c.nome !== acao.payload.nome)
+  return listaDeContratoAtual
+}
